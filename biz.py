6acb6ca977fae38e5bae64e4b3a0470c66133e7c
Philipof <93150357+Philipof@users.noreply.github.com>
2021-12-01 20:44:53 +0000

    Add files via upload

---
+++ biz.py
@@ -0,0 +1,18 @@
+choice = 0 
+
+while choice != '3':
+    print('Enter 1 to view products')
+    print('Enter 2 to view orders')
+    print('Enter 3 to exit')
+
+    choice = input('Enter your choice: ')
+
+    if choice == 1:
+        print('A')
+        #add items
+
+    if choice == 2:
+        print('B')
+        #view items
+    
+    #'A' and 'B' only there to test if menu worked
